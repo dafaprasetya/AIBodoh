7c32c8f94df883a9b6f1bcdc0c7e573f56ae79d1
dafaprasetya <49806236+dafaprasetya@users.noreply.github.com>
2020-11-28 17:22:04 +0700

    Uplot

---
+++ impup.py
@@ -0,0 +1,46 @@
+import webbrowser
+import random
+import time
+import string
+
+class style():
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    blue = '\033[34m'
+    mangenta = '\033[35m'
+    cyan = '\033[36m'
+    white = '\033[37m'
+    underline = '\033[4m'
+    reset = '\033[0m'
+wa = "https://api.whatsapp.com/send?phone=6281574999858&text="
+p = "prstya"
+user = []
+
+def whoami():
+	user = open('user/user.txt', 'r')
+	username = user.read()
+	print(username)
+	
+def help():
+	print(style.cyan + "Prstyaa command\n" + style.yellow + "1. whoami(untuk mengetahui siapa anda)\n2. help(untuk mengetahui command yang tersedia)\n3. saran(untuk mengirim saran)\n4.kirim pesan(untuk mengirim pesan via WA)"+style.reset)
+	
+def saran():
+	saran = input("Masukan Saran : ")
+	webbrowser.open_new_tab(wa + saran + " \ndari : " + user[0])
+
+
+def kirim_pesan():
+	penerima = input("masukan nomor penerima : ")
+	dari = "\ndari : " + user[0]
+	pesan = input("ketik pesan : ")
+	kirpes = "https://api.whatsapp.com/send?phone=62{}&text={}.{}".format(penerima, pesan, dari)
+	webbrowser.open_new_tab(kirpes)
+def waktu(berapa):
+	for i in range(berapa):
+		time.sleep(1)
+		print(i)
+def warna(warni):
+	war = "style" + "." +warni
+	print(war)
---
+++ tanya.py
@@ -0,0 +1,15 @@
+f = open('user/user.txt', 'r')
+h = "hello {}".format(f.read())
+menyapa = ["hai", "hello", "good morning", "selamat pagi", "pagi", "morning", "hii", "hi", "hai bot", "hai prstya","haii", "haiii", "pagii", "siang", "soree", "malamm", "hai nice to meet you", "nice to meet you"]
+jawaban_menyapa = ["hii", "hai", "hi, nice to meet you", "hai, apa kabar?", "hello", "hai saya prstya", "hi i\'m prstya", "pe", h]
+jrels = ["maaf saya tidak mengerti", "akan ku pelajari", "comman;d tidak tersedia untuk saat ini", "sorry command tidak ada"]
+
+kamu_tahu = ["kamu tahu gak?", "kamu tau ga?", "kamu tau gak?", "you know?", "kamu tau gak", "kamu tahu gak", "kamu tau ga", "kamu tahu tidak", "you know", "do you know?", "tau?"]
+jktau = ["apa?", "what?", "apaa??", "Apaa??"]
+ya = ["boleh", "y", "yes", "ya", "yess", "boleh", "boleehh"]
+tidak = ["no", "tidak","G", "g", "gak", "tidak terimakasih", "tidak", "ga mau"]
+jambaraha = ["jam berapa sekarang?", "ayena jam baraha?", "jam baraha", "sekarang jam berapa", "jam?", "tunjukan sekarang jam berapa", "jam berapa", "sekarang jam berapa?"]
+wa = ["buka wa", "open whatsapp", "buka whatsapp", "buka WA"]
+ig = ["buka ig", "open instagram", "buka instagram","buka IG"]
+fb = ["buka fb", "open facebook", "buka facebook", "buka FB"]
+#print(jawaban_menyapa)
